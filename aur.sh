2ae4ce0a6a313b0ed5e5643083aab0832f733576
Nikki <cerven0@protonmail.com>
2024-04-29 07:02:54 +0200

    did way too much stuff at once :3

--- aur.sh
+++ aur.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/bash
 
-# Download the aur package
-#
-# Try installing the aur package
-#
-# If it failed and there is a dependency, try to install the dependency via 'sudo pacman -Syu' first
-# If pacman could not find the package, recursively invoke this script and install the dependency
-# If the dependency could still not be met, abort and notify the user about the missing dependency
-#
-# If the dependency has a dependency, the script will just recursively invoke itself, until all dependencies are installed
-# 
-# 
-# TODO: add check if there are no arguments
-#
-
-
 # exit, when any command fails
 set -e
 
@@ -24,9 +9,60 @@ set -u
 # if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
 set -o pipefail
 
-# commands are printed to terminal
-# set -x
+DEBUG=false
+REMOVE=false
+QUERY=false
+SYNC=false
+operation_count=0
 
+evaluate_arguments()
+{
+	for var in $@; do
+
+		if [[ ${var:0:1} == "-" ]]; then
+
+			argument_length=${#var}
+
+			if [[ $argument_length -eq 1 ]]; then
+				printf "error: no argument specified...\n"
+				exit 1
+			fi
+
+			for ((j=1;j<=argument_length-1;j++)); do
+
+				case ${var:$j:1} in
+					"d")
+						DEBUG=true	
+						;;
+					"S")
+						SYNC=true
+						let "operation_count=operation_count+1"
+						;;
+					"Q")
+						QUERY=true
+						let "operation_count=operation_count+1"
+						;;
+					"R")
+						REMOVE=true
+						let "operation_count=operation_count+1"
+						;;
+					*)
+						printf "error: invalid option -- '${var:$j:1}'\n"
+						exit 1
+						;;
+				esac
+
+			done
+		fi
+	done
+
+	if [[ $operation_count -ge 2 ]]; then
+		printf "error: only one operation may be used at a time...\n"
+		exit 1
+	fi
+
+	exit 0
+}
 
 install()
 {
@@ -34,6 +70,7 @@ install()
 
 	makepkg -si
 
+	# TODO maybe make grep output quiet?
 	has_missing_dependency=$(cat makepkg.out | grep -Po 'Missing dependencies')
 	
 	if [[ -n ${has_missing_dependency} ]]; then
@@ -50,14 +87,28 @@ install()
 	return 1
 }
 
+download()
+{
+# if ls finds an existing package, it won't clone
+# if it doesn't it clones
+# if repo is empty, it fails and removes the empty repo	
+	{
+	    ls $1 > /dev/null 2>&1 &&
+		printf "$1 already cloned, attempting to install...\n"
+	} || {
+	    if git clone "https://aur.archlinux.org/$1.git" 2>&1 | grep -Pq 'cloned an empty repository.'; then
+	    	printf "error: $1 does not exist...\n"
+			rm -r $1
+			exit 1
+	    fi
+	}
+}
 
 main()
 {
-	cd ~/Downloads/aur
+	cd ~/.aur
 	
-	ls $1 2>/dev/null || {
-		git clone "https://aur.archlinux.org/$1.git"
-	}
+	download $1
 
 	cd $1
 	
@@ -68,9 +119,6 @@ main()
 	return 0
 }
 
-if [[ $# -eq 0 ]]; then
-	printf "No arguments given.\n"
-	exit 1
-fi
+evaluate_arguments "$@"
 
 main $1
